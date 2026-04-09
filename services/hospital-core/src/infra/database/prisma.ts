5c9a75cd59db1c6a87e33422e351970f6ffd7d28
Gabriel Rodrigues <gabrieldealmeidarodrigues@MacBook-Air-de-Gabriel.local>
2026-04-09 20:53:57 -0300

    Refac: Change to independent services structure

--- services/hospital-core/src/infra/database/prisma.ts
+++ services/hospital-core/src/infra/database/prisma.ts
@@ -1,7 +1,7 @@
-import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 import { env } from '../env';
+import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: env.DATABASE_URL,
