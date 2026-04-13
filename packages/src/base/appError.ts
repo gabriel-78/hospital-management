0343bca7a3c726b6ff2038b9a906ecb67f4c4b77
Gabriel Rodrigues <gabriel.78.rodrigues@gmail.com>
2026-04-13 19:17:06 -0300

    Feat: Add new error types

--- packages/src/base/appError.ts
+++ packages/src/base/appError.ts
@@ -1,6 +1,13 @@
 export class AppError extends Error {
   constructor(
-    public code: 'DB_ERROR' | 'NOT_FOUND' | 'DOMAIN_ERROR' | 'VALIDATION_ERROR' | 'UNEXPECTED',
+    public code:
+      | 'DB_ERROR'
+      | 'NOT_FOUND'
+      | 'DOMAIN_ERROR'
+      | 'VALIDATION_ERROR'
+      | 'UNEXPECTED'
+      | 'INTEGRATION_ERROR'
+      | 'INTEGRATION_UNAVAILABLE',
     public message: string,
     public details?: unknown,
   ) {
@@ -18,6 +25,10 @@ export class AppError extends Error {
         return 422;
       case 'DB_ERROR':
         return 500;
+      case 'INTEGRATION_ERROR':
+        return 502;
+      case 'INTEGRATION_UNAVAILABLE':
+        return 503;
       case 'UNEXPECTED':
       default:
         return 400;
